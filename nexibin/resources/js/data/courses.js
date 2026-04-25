d3c86604fa54512410d3c1110f2f3d604f6db8cd
Lowell Augusteen Cabie <cabielowellaugusteen@gmail.com>
2026-04-26 01:35:04 +0800

    Deleted all Files

--- nexibin/resources/js/data/courses.js
+++
@@ -1,9 +0,0 @@
-// resources/js/data/courses.js
-
-export const courses = [
-    { value: "BSCS", label: "BSCS - Computer Science" },
-    { value: "BSIT", label: "BSIT - Information Technology" },
-    { value: "BSEMC", label: "BSEMC - Multimedia Computing" },
-    { value: "BSIS", label: "BSIS - Information Systems" },
-    { value: "BSN", label: "BSN - Nursing" },
-];
